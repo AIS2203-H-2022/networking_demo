bb8836bba9f8cddfac6dae5897fa54c3deff03e7
Lars Ivar Hatledal <laht@ntnu.no>
2022-08-16 12:29:23 +0200

    update

--- dds/python/py_subscriber.py
+++ dds/python/py_subscriber.py
@@ -12,19 +12,24 @@ import numpy as np
 def main():
     print("Starting subscriber.")
     participant = DomainParticipant()
-    topic = Topic(participant, "HelloWorldTopic", ImageStruct)
+    topic = Topic(participant, "RaspiImageTopic", ImageStruct)
     reader = DataReader(participant, topic)
 
-    cv.namedWindow("img")
+    display = False
 
     # If we don't receive a single announcement for three seconds we want the script to exit.
-    for msg in reader.take_iter(timeout=duration(seconds=1)):
+    for msg in reader.take_iter(timeout=duration(seconds=3)):
+
+        if not display:
+            cv.namedWindow("img")
+            display = True
 
         buf = np.frombuffer(bytes(msg.data), dtype=np.uint8)
         img = cv.imdecode(buf, cv.IMREAD_COLOR)
+        #img = cv.resize(img, (640, 480))
         cv.imshow("img", img)
-        cv.waitKey(10)
-        print("Message RECEIVED")
+        cv.waitKey(1)
+        #print("Message RECEIVED")
 
 
 if __name__ == "__main__":
