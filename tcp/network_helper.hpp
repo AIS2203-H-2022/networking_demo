453a635a2e59593e5f386796c0a6c442423e06ef
Lars Ivar Hatledal <laht@ntnu.no>
2022-11-03 12:11:15 +0100

    refactor

--- tcp/network_helper.hpp
+++ tcp/network_helper.hpp
@@ -17,10 +17,10 @@ std::array<unsigned char, 4> int_to_bytes(int n) {
 }
 
 int bytes_to_int(std::array<unsigned char, 4> buffer) {
-    return int((unsigned char) (buffer[0]) << 24 |
-               (unsigned char) (buffer[1]) << 16 |
-               (unsigned char) (buffer[2]) << 8 |
-               (unsigned char) (buffer[3]));
+    return int(buffer[0] << 24 |
+               buffer[1] << 16 |
+               buffer[2] << 8 |
+               buffer[3]);
 }
 
 #endif//NETWORKING_DEMO_NETWORK_HELPER_HPP
